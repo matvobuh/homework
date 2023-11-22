3fd8a014a68727b7458a6e3329a784b4f29843b6
matvobuh <145449701+matvobuh@users.noreply.github.com>
2023-11-22 11:42:50 +0200

    Update orderSummary.js

--- orderSummary.js
+++ orderSummary.js
@@ -1,27 +1,27 @@
-const tellimus = {
-  tellimuseKaubad: [
-    { nimi: "Toode 1", kogus: 2, hind: 15.99 },
-    { nimi: "Toode 2", kogus: 1, hind: 29.99 },
-    { nimi: "Toode 3", kogus: 3, hind: 9.99 }
-  ]
-};
-
-function printTellimus(tellimus) {
-  let kogusumma = 0;
-
-  tellimus.tellimuseKaubad.forEach((kaup, index) => {
-    const summa = kaup.kogus * kaup.hind;
-    kogusumma += summa;
-
-    console.log(`Kaubanimi: ${kaup.nimi}`);
-    console.log(`Kogus: ${kaup.kogus}`);
-    console.log(`Hind ühe tüki kohta: ${kaup.hind}`);
-    console.log(`Summa: ${summa}`);
-    console.log('\n'); 
-  });
-
-  console.log(`Tellimuse kogusumma: ${kogusumma}`);
+function calculatePriceWithVAT(price, vat) {
+  return price * (1 + vat);
 }
 
 
-printTellimus(tellimus);
+let totalAmount = 0;
+
+
+tellimus.rows.forEach(function(item) {
+
+  console.log("Nimi: " + item.name);
+  console.log("Kogus: " + item.amount);
+  console.log("Hind: " + item.price.toFixed(2) + "€");
+  
+
+  const priceWithVAT = calculatePriceWithVAT(item.price, item.vat);
+  
+
+  console.log("Summa: " + (priceWithVAT * item.amount).toFixed(2) + "€");
+  console.log("------------------------");
+  
+
+  totalAmount += priceWithVAT * item.amount;
+});
+
+
+console.log("Tellimuse kogusumma: " + totalAmount.toFixed(2) + "€");
